19cb4005097ac5ed5d12f5f92b283007f4af42fe
javiermunozalonso <jamual90@gmail.com>
2022-12-07 01:45:20 +0100

    updated the main script and the model_builder

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow.keras.optimizers import Adam
-
-import logging
+import tensorflow as tf
 
 from dataset import create_train_test_validation_dataset
 from model.model_builder import ModelBuilder
+
+import logging
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
 LEARNING_RATE=0.0001
 
 def run():
     logging.info('Init run')
-    [train_dataset, validation_dataset, test_dataset] = create_train_test_validation_dataset
-    
+    [train_dataset, validation_dataset, test_dataset] = create_train_test_validation_dataset()
+
     model_builder = ModelBuilder(train_dataset=train_dataset,
                                     test_dataset=test_dataset,
                                     validation_dataset=validation_dataset,
                                     loss=BinaryCrossentropy(),
-                                    optimizer=Adam(learning_rate=LEARNING_RATE),
-    )
-    
+                                    optimizer=Adam(learning_rate=LEARNING_RATE))
+
     model_builder.build()
-    
     history = model_builder.train()
-    
+
     logging.info(history)
-    
+
     evaluate_results = model_builder.evaluate()
-    
+
     logging.info(evaluate_results)
-    
     logging.info('end run')
     return
 
--- model/model_builder.py
+++ model/model_builder.py
@@ -16,22 +16,39 @@ class ModelBuilder:
     train_dataset: List[str]
     test_dataset: List[str]
     validation_dataset: List[str]
-    _model: tf.keras.Model
-    # model_path: str
-    optimizer: tf.keras.optimizers.Optimizer
-    loss: tf.keras.losses.Loss
-    
-    _model_name: str = 'model'
-    
+    optimizer: tf.keras.optimizers.Optimizer = None
+    loss: tf.keras.losses.Loss = None
+    _model: tf.keras.Model = None
+    _model_name: str = None
     _batch_size: int = BATCH_SIZE
-    callbacks: List[tf.keras.callbacks.Callback] = None
-    metrics: List[tf.keras.metrics.Metric] = [tf.keras.metrics.Metric.binary_accuracy]
     _epochs: int = GLOBAL_EPOCHS
-    # class_weights: Dict[int, float] = None
-    class_weights: Dict[int, float] = {
-        ClassificationNames.PNEUMONIA.value: 1.0,
-        ClassificationNames.NORMAL.value: 0.5
-    }
+
+    @property
+    def callbacks(self) -> List[tf.keras.callbacks.Callback]:
+        return [tf.keras.callbacks.TensorBoard('model_logs')]
+
+    @property
+    def metrics(self) -> List[tf.keras.metrics.Metric]:
+        return [
+                tf.keras.metrics.BinaryAccuracy(),
+                tf.keras.metrics.BinaryCrossentropy(),
+                tf.keras.metrics.BinaryIoU(),
+                tf.keras.metrics.Precision(),
+                tf.keras.metrics.Recall(),
+                tf.keras.metrics.AUC(name='AUC_ROC',curve='ROC'),
+                tf.keras.metrics.AUC(name='AUC_PR',curve='PR'),
+                tf.keras.metrics.FalseNegatives(),
+                tf.keras.metrics.FalsePositives(),
+                tf.keras.metrics.TrueNegatives(),
+                tf.keras.metrics.TruePositives()
+                ]
+
+    @property
+    def class_weights(self) -> Dict[int, float]:
+        return {
+                ClassificationNames.PNEUMONIA.value: 1.0,
+                ClassificationNames.NORMAL.value: 0.5
+                }
 
     @property
     def model_path(self) -> str:
@@ -55,13 +72,9 @@ class ModelBuilder:
 
     @property
     def model_name(self) -> str:
+        if not self._model_name:
+            self._model_name = f'model_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
         return self._model_name
-
-    @model_name.setter
-    def set_model_name(self, stamp: str):
-        if not stamp:
-            stamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-        self._model_name = f'model_{stamp}'
 
     def add_metrics(self):
         self.metrics = list(set(self.metrics.append(tf.keras.metrics.binary_accuracy)))
@@ -70,21 +83,18 @@ class ModelBuilder:
 
         inputs = tf.keras.layers.Input(shape=INPUT_SHAPE)
 
-        # Block One
         x = tf.keras.layers.Conv2D(filters=16, kernel_size=3, padding='valid')(inputs)
         x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.Activation('relu')(x)
         x = tf.keras.layers.MaxPool2D()(x)
         x = tf.keras.layers.Dropout(0.1)(x)
 
-        # Block Two
         x = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding='valid')(x)
         x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.Activation('relu')(x)
         x = tf.keras.layers.MaxPool2D()(x)
         x = tf.keras.layers.Dropout(0.2)(x)
 
-        # Block Three
         x = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='valid')(x)
         x = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding='valid')(x)
         x = tf.keras.layers.BatchNormalization()(x)
@@ -92,13 +102,11 @@ class ModelBuilder:
         x = tf.keras.layers.MaxPool2D()(x)
         x = tf.keras.layers.Dropout(0.4)(x)
 
-        # Head
         x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.Flatten()(x)
         x = tf.keras.layers.Dense(128, activation='relu')(x)
         x = tf.keras.layers.Dropout(0.5)(x)
 
-        #Final Layer (Output)
         output = tf.keras.layers.Dense(1, activation='sigmoid')(x)
 
         self._model = tf.keras.Model(inputs=[inputs], outputs=output, name=self.model_name)
@@ -111,46 +119,21 @@ class ModelBuilder:
         return self._model
 
     def train(self):
-        history_fit = self._model.fit(x=self.train_dataset.dataset,
+        history_fit = self._model.fit(x=self.train_dataset,
                         epochs=self.epochs,
-                        batch_size=self._batch_size,
-                        validation_data=self.validation_dataset.dataset,
+                        batch_size=self.batch_size,
+                        validation_data=self.test_dataset,
                         callbacks=self.callbacks,
                         class_weight=self.class_weights)
         self._model.save(self.model_path)
         return history_fit
 
-    def evaluate(self) -> Tuple[float, float]:
-        loss, accuracy = self._model.evaluate(x=self.test_dataset.dataset,
-                                                batch_size=self._batch_size)
-        return loss, accuracy
+    def evaluate(self):
+        return self._model.evaluate(x=self.validation_dataset,
+                                                batch_size=self.batch_size)
 
-    def predict_model(self, image_path: str) -> Tuple[int, float]:
-        # image = tf.keras.preprocessing.image.load_img(image_path,
-        #                                                 target_size=(224, 224))
-        # image = tf.keras.preprocessing.image.img_to_array(image)
-        # image = np.expand_dims(image, axis=0)
-        # image = tf.keras.applications.mobilenet.preprocess_input(image)
-        prediction = self._model.predict(image_path)
-        return np.argmax(prediction),\
-                np.max(prediction),\
-                ClassificationNames.to_array[np.argmax(prediction)]
-
-    # def predict_model_from_dataset(self, dataset: ImagesDataset) -> Tuple[int, float]:
-    #     image_path = dataset.get_random_image_path()
-    #     return self.predict_model(image_path)
-
-    # def predict_model_from_test_dataset(self) -> Tuple[int, float]:
-    #     return self.predict_model_from_dataset(self.test_dataset.normal_images_dataset)
-
-    # def predict_model_from_validation_dataset(self) -> Tuple[int, float]:
-    #     return self.predict_model_from_dataset(self.validation_dataset.normal_images_dataset)
-
-    # def predict_model_from_train_dataset(self) -> Tuple[int, float]:
-    #     return self.predict_model_from_dataset(self.train_dataset.normal_images_dataset)
-
-    # def predict_model_from_pneumonia_test_dataset(self) -> Tuple[int, float]:
-    #     return self.predict_model_from_dataset(self.test_dataset.pneumonia_images_dataset)
-
-    # def predict_model_from_pneumonia_validation_dataset(self) -> Tuple[int, float]:
-    #     return self.predict_model_from_dataset(self.validation_dataset.pneumonia_images_dataset)
+    # def predict_model(self, image_path: str) -> Tuple[int, float]:
+    #     prediction = self._model.predict(image_path)
+    #     return np.argmax(prediction),\
+    #             np.max(prediction),\
+    #             ClassificationNames.to_array[np.argmax(prediction)]
