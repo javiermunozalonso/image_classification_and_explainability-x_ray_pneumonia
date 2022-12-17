e4c587bcb2b6964424667d6ed8b9ac4f2932a9c2
javiermunozalonso <jamual90@gmail.com>
2022-12-17 20:49:39 +0100

    Updated model handler

--- model/model_handler.py
+++ model/model_handler.py
@@ -1,14 +1,10 @@
 from dataclasses import dataclass, field
 from datetime import datetime
-from typing import Dict, List, Tuple
-from dataset import DatasetFolder, ImagesDataset
-from config import ClassificationNames
+from typing import Dict, List
+from hyperopt import STATUS_OK, fmin, Trials, tpe
 
 import tensorflow as tf
-import numpy as np
 
-GLOBAL_EPOCHS = 30
-# BATCH_SIZE = 8
 INPUT_SHAPE = (220, 220, 3) # color_mode = rgb
 
 @dataclass
@@ -18,11 +14,11 @@ class ModelHandler:
     validation_dataset: List[str]
     optimizer: tf.keras.optimizers.Optimizer = None
     loss: tf.keras.losses.Loss = None
-    __model: tf.keras.Model = None
-    __model_name: str = None
-    __batch_size: int = field(init=True, default=None, repr=False)
-    __epochs: int = field(init=True, default=None, repr=False)
-    __class_weights: Dict[int, float] = None # GLOBAL_EPOCHS
+    _model: tf.keras.Model = None
+    _model_name: str = None
+    _batch_size: int = field(init=True, default=None, repr=False)
+    _epochs: int = field(init=True, default=None, repr=False)
+    _class_weights: Dict[int, float] = None
 
     @property
     def callbacks(self) -> List[tf.keras.callbacks.Callback]:
@@ -46,19 +42,19 @@ class ModelHandler:
 
     @property
     def batch_size(self) -> int:
-        return self.__batch_size
+        return self._batch_size
 
     @batch_size.setter
     def batch_size(self, new_batch_size: int) -> None:
-        self.__batch_size = new_batch_size
+        self._batch_size = new_batch_size
 
     @property
     def class_weights(self) -> Dict[int, float]:
-        return self.__class_weights
+        return self._class_weights
 
     @class_weights.setter
     def class_weights(self, new_class_weights: Dict[int, float]) -> None:
-        self.__class_weights = new_class_weights
+        self._class_weights = new_class_weights
 
     @property
     def model_path(self) -> str:
@@ -66,20 +62,17 @@ class ModelHandler:
 
     @property
     def epochs(self) -> int:
-        return self.__epochs
+        return self._epochs
 
     @epochs.setter
     def epochs(self, new_epochs: int) -> None:
-        self.__epochs = new_epochs
+        self._epochs = new_epochs
 
     @property
     def model_name(self) -> str:
-        if not self.__model_name:
-            self.__model_name = f'model_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
-        return self.__model_name
-
-    def add_metrics(self):
-        self.metrics = list(set(self.metrics.append(tf.keras.metrics.binary_accuracy)))
+        if not self._model_name:
+            self._model_name = f'model_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
+        return self._model_name
 
     def build(self) -> tf.keras.Model:
 
@@ -111,27 +104,30 @@ class ModelHandler:
 
         output = tf.keras.layers.Dense(1, activation='sigmoid')(x)
 
-        self.__model = tf.keras.Model(inputs=[inputs], outputs=output, name=self.model_name)
+        self._model = tf.keras.Model(inputs=[inputs], outputs=output, name=self.model_name)
 
-        self.__model.compile(optimizer=self.optimizer,
+        self._model.compile(optimizer=self.optimizer,
                             loss=self.loss,
                             metrics=self.metrics)
 
-        self.__model.summary()
-        return self.__model
+        self._model.summary()
+        return self._model
 
     def train(self):
-        history_fit = self.__model.fit(x=self.train_dataset,
+        history_fit: tf.keras.callbacks.History = self._model.fit(x=self.train_dataset,
                         epochs=self.epochs,
                         batch_size=self.batch_size,
                         validation_data=self.test_dataset,
                         callbacks=self.callbacks,
                         class_weight=self.class_weights)
-        self.__model.save(self.model_path)
+
         return history_fit
 
+    def save_model(self):
+        self._model.save(self.model_path)
+
     def evaluate(self):
-        evaluate_results = self.__model.evaluate(x=self.validation_dataset,
+        evaluate_results = self._model.evaluate(x=self.validation_dataset,
                                                 batch_size=self.batch_size)
-        # evaluation_metrics_names =
-        return dict(zip(self.__model.metrics_names, evaluate_results))
+        evaluation_metrics_names = list(map(lambda name: ''.join(['evaluation_', name]), self._model.metrics_names))
+        return dict(zip(evaluation_metrics_names, evaluate_results))
