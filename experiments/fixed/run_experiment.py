0c96de85a9deffec0e1c3457230ca1078dd21931
javiermunozalonso <jamual90@gmail.com>
2022-12-17 20:48:39 +0100

    Created experiment for a fixed and normal training

---
+++ experiments/fixed/run_experiment.py
@@ -0,0 +1,65 @@
+import logging
+
+from config import ClassificationNames
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
+import pprint
+
+from dataset import create_train_test_validation_dataset
+from model.model_handler import ModelHandler
+
+import time
+
+import mlflow
+
+from utils.create_confusion_matrix_artifact import create_confusion_matrix_artifact
+
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
+
+def run_experiment():
+    logging.info('Init run')
+    [train_dataset, validation_dataset, test_dataset] = create_train_test_validation_dataset()
+
+    learning_rate: float = 1.e-8
+    batch_size: int = 8
+    epochs: int = 10
+    pneumonia_weight: float = .5
+    normal_weight: float = 1
+
+    model_handler = ModelHandler(train_dataset = train_dataset,
+                                    test_dataset = test_dataset,
+                                    validation_dataset = validation_dataset,
+                                    loss = BinaryCrossentropy(),
+                                    optimizer = Adam(learning_rate = learning_rate))
+
+    model_handler.batch_size = batch_size
+    model_handler.epochs = epochs
+    model_handler.class_weights = {
+                                    ClassificationNames.PNEUMONIA.value: pneumonia_weight,
+                                    ClassificationNames.NORMAL.value: normal_weight
+                                }
+
+    model_handler.build()
+
+    train_results = model_handler.train()
+
+    logging.info(train_results)
+    pprint.pprint(train_results.history)
+    model_handler.save_model()
+
+    evaluate_results = model_handler.evaluate()
+
+    pprint.pprint(evaluate_results)
+    print(evaluate_results)
+
+    mlflow.log_metrics(evaluate_results)
+    mlflow.log_dict(evaluate_results, artifact_file='evaluation_results.json')
+
+    test_confusion_matrix_figure = create_confusion_matrix_artifact(true_positive=evaluate_results['evaluation_true_positives'],
+                                                        true_negative=evaluate_results['evaluation_true_negatives'],
+                                                        false_positive=evaluate_results['evaluation_false_positives'],
+                                                        false_negative=evaluate_results['evaluation_false_negatives'])
+
+    mlflow.log_figure(test_confusion_matrix_figure, artifact_file='test_confusion_matrix.png')
+    logging.info('end run')
